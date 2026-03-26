8310698de67d368dd3cbb8fcb45fe731552d969c
mari <mariana.casilva0633@gmail.com>
2026-03-26 12:20:40 -0300

    formulario, email e senha

---
+++ aula06/processa.php
@@ -0,0 +1,17 @@
+<?php
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    //email: adm@gmail.com
+    //senha: 1234
+
+    if ($email == 'adm@gmail.com' && $senha == '1234') {
+        //echo "vamos para a area restrita";
+        $nome = "Mariana";
+        header('Location: restrita.php?nome='.$nome);
+    } else {
+        //echo "volte para o formulario";
+        header('Location: formulario.php');
+    }
+    
+?>
---
+++ aula06/restrita.php
@@ -0,0 +1,14 @@
+<?php
+    $nome = $_GET['nome'];
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>area restrita</title>
+</head>
+<body>
+    <p>Bem-vindo <?php echo $nome; ?> a sua área restrita</P>
+</body>
+</html>

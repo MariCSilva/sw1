5877cb753cdbc82b10aeaa659137809a723ac7a3
mari <mariana.casilva0633@gmail.com>
2026-05-28 12:16:10 -0300

    criar vetor par

--- exercicios_array/exercicio9.php
+++ exercicios_array/exercicio9.php
@@ -1,12 +1,17 @@
 <?php
     echo"*lista: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] <br><br>";
     $num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    $pares = [];
     foreach ($num as $valor) {
         if ($valor %2 == 0) {
             echo"$valor é par <br>";
+            $pares[] = $valor;
         } else {
             echo"$valor é impar <br>";
         }
-        ;
     }
+
+    echo "<br> Lista de pares: ";
+    foreach ($pares as $valor) {
+        echo "$valor, ";}
 ?>

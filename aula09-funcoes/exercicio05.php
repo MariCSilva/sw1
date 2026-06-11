02fca5eabd50158e0c8595b779d6cfab4ee3eba5
mari <mariana.casilva0633@gmail.com>
2026-06-11 12:28:46 -0300

    Add files via upload

---
+++ aula09-funcoes/exercicio05.php
@@ -0,0 +1,7 @@
+<?php
+    $temperaturas = [22.5, 24.0, 25.5, 23.0];
+
+    functioncalcularMediaTemperatura($listaDeTemperaturas){
+        $soma = array_sum($listaDeTemperaturas) / count($listaDeTemperaturas);
+    }
+?>
